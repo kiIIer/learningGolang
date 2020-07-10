1f644e647491a9a251e777df4e4c57d3ad68a41c
Mike <michael.molchanov.2004@gmail.com>
2020-07-10 15:17:18 +0300

    changed location

---
+++ slices-exercise/slices-exercise.go
@@ -0,0 +1,20 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	s := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		s[i] = make([]uint8, dx)
+	}
+	for i := 0; i < dy; i++ {
+		for j := 0; j < dy; j++ {
+			s[i][j] = uint8(i)
+		}
+	}
+	return s
+}
+
+func main() {
+	pic.Show(Pic)
+}
