5da043c86fa2ec32010aecbd659eff386fd17b48
Mike <michael.molchanov.2004@gmail.com>
2020-07-14 12:55:02 +0300

    done with interfaces

--- area-calculator/main.go
+++ area-calculator/main.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
+type triangle struct {
+	width, hight float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type rectangle struct {
+	side1, side2 float64
+}
+
+func (t triangle) calculateArea() float64 {
+	return t.width * t.hight / 2
+}
+func (c circle) calculateArea() float64 {
+	return c.radius * c.radius * math.Pi
+}
+func (r rectangle) calculateArea() float64 {
+	return r.side1 * r.side2
+}
+
+type areaCalculator interface {
+	calculateArea() float64
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	s := make([]interface{}, 0, 10)
+	s := make([]areaCalculator, 0, 10)
 	fmt.Println(s)
 	for i := 0; i < 10; i++ {
-		seT := Triangle{float64(rand.Intn(10) + 1), float64(rand.Intn(10) + 1)}
-		seC := Circle{float64(rand.Intn(10) + 1)}
-		seR := Rectangle{float64(rand.Intn(10) + 1), float64(rand.Intn(10) + 1)}
+		seT := triangle{float64(rand.Intn(10) + 1), float64(rand.Intn(10) + 1)}
+		seC := circle{float64(rand.Intn(10) + 1)}
+		seR := rectangle{float64(rand.Intn(10) + 1), float64(rand.Intn(10) + 1)}
 		rndn := rand.Intn(3)
 		switch {
 		case rndn == 0:
@@ -26,22 +53,10 @@ func main() {
 
 	}
 	fmt.Printf("%+v\n", s)
-	for i := 0; i < len(s); i++ {
+	for i := range s {
 		ct := s[i]
-		t, okT := ct.(Triangle)
-		c, okC := ct.(Circle)
-		r, _ := ct.(Rectangle)
-		switch {
-		case okT:
-			a := t.calculateArea()
-			fmt.Printf("%+v area: %.2f\n", ct, a)
-		case okC:
-			a := c.calculateArea()
-			fmt.Printf("%+v area: %.2f\n", ct, a)
-		default:
-			a := r.calculateArea()
-			fmt.Printf("%+v area: %.2f\n", ct, a)
-		}
+		a := ct.calculateArea()
+		fmt.Printf("%+v area: %.2f\n", ct, a)
 
 	}
 }
