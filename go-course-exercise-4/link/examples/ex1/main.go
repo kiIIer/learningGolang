270199dd2216f8a742de05ac5135040cdb497323
Mike <michael.molchanov.2004@gmail.com>
2020-08-19 11:18:47 +0300

    done exercise 4

---
+++ go-course-exercise-4/link/examples/ex1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kiIIer/learningGolang/go-course-exercise-4/link"
+)
+
+var exampleHtml = `
+<html>
+<body>
+  <h1>Hello!</h1>
+  <a href="/other-page">A link to another page</a>
+</body>
+</html>
+
+`
+
+func main() {
+	r := strings.NewReader(exampleHtml)
+	links, err := link.Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
+}
