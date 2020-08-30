9f92fa649400dacb65288d3fc5b41449ad63da31
Mike <michael.molchanov.2004@gmail.com>
2020-08-30 09:14:32 +0300

    done task manager

--- go-course-exercise-6/task/cmd/do.go
+++ go-course-exercise-6/task/cmd/do.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/kiIIer/learningGolang/go-course-exercise-6/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -20,6 +21,25 @@ var doCmd = &cobra.Command{
 				ids = append(ids, id)
 			}
 		}
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong:", err)
+			return
+		}
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-1]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\" as completed. Error %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\" as completed", id)
+			}
+		}
+
 		fmt.Println(ids)
 	},
 }
--- go-course-exercise-6/task/main.go
+++ go-course-exercise-6/task/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/kiIIer/learningGolang/go-course-exercise-6/task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/kiIIer/learningGolang/go-course-exercise-6/task/cmd"
+	"github.com/kiIIer/learningGolang/go-course-exercise-6/task/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.RootCmd.Execute()
+	home, _ := homedir.Dir()
+	dbPath := filepath.Join(home, "tasks.db")
+	must(db.Init(dbPath))
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
